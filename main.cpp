5a133e91d7493f4f7e0778b6d03abca28f6a31fe
Kiselev Vladislav <kiselev.vladislav@urfu.me>
2021-12-24 13:07:39 +0500

    Add commentaries and error handling

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <map>
 
-std::map<int, int> distance;
-std::map<int, int> cost;
-std::map<int, int> answer;
+std::map<int, int> distance; // Ассоциативный автоматически сортируемый динамический массив для данных о растояниях до домов сотрудников
+std::map<int, int> cost;     // Ассоциативный автоматически сортируемый динамический массив для данных о стоимости за километр пути
+std::map<int, int> answer;   // Ассоциативный автоматически сортируемый динамический массив для вывода ответа
 
-int arr[1000][2] = {};
+int arr[1000][2] = {};       // Промежуточный массив
 
 int N;
 
-template <typename A, typename B>
+template <typename A, typename B> // Структура выполняющая обмен местами ключа и значения в ассоциативном массиве
 std::multimap<B, A> flip_map(std::map<A,B> & src) {
 
     std::multimap<B,A> dst;
@@ -22,38 +22,40 @@ std::multimap<B, A> flip_map(std::map<A,B> & src) {
 
 int main() {
     std::cout << "Enter the number of employees: ";
-    std::cin >> N;
+    std::cin >> N; // ввод числа сотрудников
 
     for (int i = 0; i < N; i++){
-        std::cin >> distance[i];
+        std::cin >> distance[i]; // Заполнение ассоциативного массива данными о растоянии до домов сотрудников
     }
 
     for (int i = 0; i < N; i++){
-        std::cin >> cost[i];
+        std::cin >> cost[i];  // Заполнение ассоциативного массива данными о стоимости за километр
     }
 
-    std::multimap<int, int> distanceSorted = flip_map(distance);
+    std::multimap<int, int> distanceSorted = flip_map(distance); // Разворот и пересортировка ассоциативного массива
     std::multimap<int, int> costSorted = flip_map(cost);
 
     int i = 0;
-    for(auto it = distanceSorted.rbegin(); it != distanceSorted.rend(); ++it){
+    for(auto it = distanceSorted.rbegin(); it != distanceSorted.rend(); ++it){ // составление решения задачи, заполнение промежуточного массива в обратном порядке дистанциями
         arr[i][0] = it->second;
         i++;
     }
 
     int i1 = 0;
-    for(const auto & it : costSorted) {
+    for(const auto & it : costSorted) { // составление решения задачи, заполнение промежуточного массива в прямом порядке стоимостью
         arr[i1][1] = it.second;
         i1++;
     }
 
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++){ // автосортировка результирующего набора
         answer[arr[i][0]] = arr[i][1];
     }
 
-    for (const auto & item : answer) {
+    for (const auto & item : answer) { // вывод решения
         std::cout << item.second + 1 << " ";
     }
+
+    // Защита от некоректного ввода реализованна строгой типизацией ожидаемых данных
 
     return 0;
 }
